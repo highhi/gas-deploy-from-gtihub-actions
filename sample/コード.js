aa38ac146b422211f9d262808582f9eacc7279a1
highhi <kazu8160@gmail.com>
2020-07-12 01:37:51 +0900

    edit 3

--- sample/コード.js
+++ sample/コード.js
@@ -1,5 +1,5 @@
 function doGet(e) {
-  payload = JSON.stringify({ message: 'hello' })
+  payload = JSON.stringify({ message: 'hello world' })
   const output = ContentService.createTextOutput();
   output.setMimeType(ContentService.MimeType.JSON);
   output.setContent(payload);
